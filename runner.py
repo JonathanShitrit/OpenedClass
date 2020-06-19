1126b06bacea9477fb633a846903ce4ad7086929
Jonathan Shitrit <Jonathanshitrit8@gmail.com>
2020-06-19 14:39:39 -0400

    try exception

--- runner.py
+++ runner.py
@@ -23,69 +23,69 @@ import os
 classStatus = "Closed"
 
 while(True):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_PATH")
+    try:
+        print("reopening window...")
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_PATH")
 
-    driver = webdriver.Chrome(
-        executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+        driver = webdriver.Chrome(
+            executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
-    driver.get("https://globalsearch.cuny.edu/CFGlobalSearchTool/search.jsp")
+        driver.get("https://globalsearch.cuny.edu/CFGlobalSearchTool/search.jsp")
 
-    # initial page
-    college = driver.find_element_by_id("QNS01")
-    college.click()
-    term = driver.find_element_by_xpath("//*[contains(text(), 'Fall Term')]")
-    term.click()
-    nextBtn = driver.find_element_by_class_name("SSSBUTTON_CONFIRMLINK")
-    nextBtn.click()
+        # initial page
+        college = driver.find_element_by_id("QNS01")
+        college.click()
+        term = driver.find_element_by_xpath(
+            "//*[contains(text(), 'Fall Term')]")
+        term.click()
+        nextBtn = driver.find_element_by_class_name("SSSBUTTON_CONFIRMLINK")
+        nextBtn.click()
 
-    # second page
-    subject = driver.find_element_by_xpath(
-        "//*[contains(text(), 'Computer Science')]")
-    subject.click()
-    courseCareer = driver.find_element_by_xpath(
-        "//*[contains(text(), 'Undergraduate')]")
-    courseCareer.click()
-    showOpenClass = driver.find_element_by_id("open_classId")
-    if(showOpenClass.is_selected()):
-        showOpenClass.click()
-    searchBtn = driver.find_element_by_id("btnGetAjax")
-    searchBtn.click()
+        # second page
+        subject = driver.find_element_by_xpath(
+            "//*[contains(text(), 'Computer Science')]")
+        subject.click()
+        courseCareer = driver.find_element_by_xpath(
+            "//*[contains(text(), 'Undergraduate')]")
+        courseCareer.click()
+        showOpenClass = driver.find_element_by_id("open_classId")
+        if(showOpenClass.is_selected()):
+            showOpenClass.click()
+        searchBtn = driver.find_element_by_id("btnGetAjax")
+        searchBtn.click()
 
-    # list of classes page
-    sections = ["48158", "48201", "51617", "48196", "48220"]
-    for section in sections:
-        driver.find_element_by_id("imageDivLink_inst0").click()
-        # find a way to choose dropdown by name and not id
-        driver.find_element_by_id("imageDivLink21").click()
+        # list of classes page
+        sections = ["48158", "48201", "51617", "48196", "48220"]
+        for section in sections:
+            driver.find_element_by_id("imageDivLink_inst0").click()
+            # find a way to choose dropdown by name and not id
+            driver.find_element_by_id("imageDivLink21").click()
 
-        driver.find_element_by_xpath(
-            "//*[contains(text(), '" + section + "')]").click()
+            driver.find_element_by_xpath(
+                "//*[contains(text(), '" + section + "')]").click()
 
-        print("checking status...")
-        status = driver.find_element_by_id("SSR_CLS_DTL_WRK_SSR_DESCRSHORT")
-        classStatus = status.text
-        print("class is", classStatus)
+            print("checking status...")
+            status = driver.find_element_by_id(
+                "SSR_CLS_DTL_WRK_SSR_DESCRSHORT")
+            classStatus = status.text
+            print("class is", classStatus)
 
-        if(classStatus == "Open"):
-            myclass = test.sms()
-            myclass.sendSmsTo("3474663815", section)
-        # time.sleep(50)
-        driver.back()
+            if(classStatus == "Open"):
+                myclass = test.sms()
+                myclass.sendSmsTo("3474663815", section)
+            # time.sleep(50)
+            driver.back()
 
-    time.sleep(5)
-    print("killing chrome...")
-    stream = os.popen("pkill --oldest chrome")
-    time.sleep(10)
-
-    # stream = os.popen(
-    #     "kill $(ps aux | grep google | grep -v grep | awk '{print $2}')")
-    # output = stream.read()
-    # print(output)
-    # time.sleep(5)
+        time.sleep(5)
+        print("closing window...")
+        driver.close()
+        time.sleep(5)
+    except Exception as e:
+        print(e)
 
 
 # CSCI 335-31
