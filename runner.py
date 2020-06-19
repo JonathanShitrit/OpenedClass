17da5e4d10f84c76a1da0b62fc5195101f9037c5
Jonathan Shitrit <Jonathanshitrit8@gmail.com>
2020-06-19 02:41:48 -0400

    update

--- runner.py
+++ runner.py
@@ -24,6 +24,7 @@ classStatus = "Closed"
 # while(True):
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_PATH")
